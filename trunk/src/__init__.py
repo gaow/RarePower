5db99626c0976232f49bab185cba471fa8a8b872
gaow <gaow@bc563b8c-44cf-4d06-a406-de582991e37c>
2012-04-27 16:08:25 +0000

    rollback to revision 37

--- trunk/src/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-ver = sys.version_info
-if (ver[0] == 2 and (ver[1], ver[2]) < (6, 2)) or (ver[0] == 3 and (ver[1], ver[2]) < (2, 3)):
-    raise Exception("The program does not support your Python version (%s). Please upgrade to Python2.6.2+ or Python 3.2.3+." % (sys.version.split()[0]))
